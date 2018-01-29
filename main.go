413212c54533b519ce36723f076328de02e93851
Göcs Jëss <laurengocs.1@gmail.com>
2018-01-29 16:10:44 +0800

    added static data

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
-    "github.com/gorilla/mux"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // our main function
 func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/people", GetPeople).Methods("GET")
-    router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-    router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-    router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8000", router))
-}
+	router := mux.NewRouter()
+	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
 
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+}
