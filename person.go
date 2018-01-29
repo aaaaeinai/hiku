34740fbcb67b5abc5cad90e6e094b488f24f4010
Göcs Jëss <laurengocs.1@gmail.com>
2018-01-29 16:01:07 +0800

    defined initial comments

--- person.go
+++ person.go
@@ -2,12 +2,15 @@ package main
 
 import "net/http"
 
+// Person ...
 type Person struct {
 	ID        string   `json:"id,omitempty"`
 	Firstname string   `json:"firstname,omitempty"`
 	Lastname  string   `json:"lastname,omitempty"`
 	Address   *Address `json:"address,omitempty"`
 }
+
+// Address ...
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
@@ -15,7 +18,14 @@ type Address struct {
 
 var people []Person
 
-func GetPeople(w http.ResponseWriter, r *http.Request)    {}
-func GetPerson(w http.ResponseWriter, r *http.Request)    {}
+// GetPeople ...
+func GetPeople(w http.ResponseWriter, r *http.Request) {}
+
+// GetPerson ...
+func GetPerson(w http.ResponseWriter, r *http.Request) {}
+
+// CreatePerson ...
 func CreatePerson(w http.ResponseWriter, r *http.Request) {}
+
+// DeletePerson ...
 func DeletePerson(w http.ResponseWriter, r *http.Request) {}
