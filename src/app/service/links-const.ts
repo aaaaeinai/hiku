8878d9c053fec2664da3dfbf9f27a35391a4b9db
Göcs Jëss <laurengocs.1@gmail.com>
2018-02-03 19:23:10 +0800

    added http argument

---
+++ src/app/service/links-const.ts
@@ -0,0 +1,25 @@
+// export const OSU_API = 'https://osu.ppy.sh'
+export const FAKE_OSU_API = 'http://localhost:3000'
+
+// export const BEATMAPS = '/api/get_beatmaps'
+export const FAKE_BEATMAPS = '/beatmaps'
+
+// export const MATCHES = '/api/get_match'
+export const FAKE_MATCHES = '/matches'
+
+// single 2 object dictionary
+// export const REPLAYS = '/api/get_replay'
+export const FAKE_REPLAYS = '/replays'
+
+// export const SCORES = '/api/get_scores'
+export const FAKE_SCORES = '/scores'
+
+// export const USER_BESTS = '/api/get_user_best'
+export const FAKE_USER_BESTS = '/user_bests'
+
+// export const USER_RECENTS = '/api/get_user_recent'
+export const FAKE_USER_RECENTS = '/user_recents'
+
+// export const USERS = '/api/get_user'
+export const FAKE_USERS = '/users'
+
