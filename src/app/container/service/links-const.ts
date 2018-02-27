ecaa71f4c1ea893dbdfe3ccd4e719bbe7baf852f
Göcs Jëss <aaaaeinai@gmail.com>
2018-02-27 17:56:26 +0800

    updated link

--- src/app/container/service/links-const.ts
+++ src/app/container/service/links-const.ts
@@ -5,25 +5,25 @@
 // ```
 export const OSU_API = 'http://localhost:3000'
 
-export const BEATMAPS = '/api/get_beatmaps'
-// export const BEATMAPS = '/beatmaps'
+// export const BEATMAPS = '/api/get_beatmaps'
+export const BEATMAPS = '/beatmaps'
 
-export const MATCHES = '/api/get_match'
-// export const MATCHES = '/matches'
+// export const MATCHES = '/api/get_match'
+export const MATCHES = '/matches'
 
 // single 2 object dictionary
-export const REPLAYS = '/api/get_replay'
-// export const REPLAYS = '/replays'
+// export const REPLAYS = '/api/get_replay'
+export const REPLAYS = '/replays'
 
-export const SCORES = '/api/get_scores'
-// export const SCORES = '/scores'
+// export const SCORES = '/api/get_scores'
+export const SCORES = '/scores'
 
-export const USER_BESTS = '/api/get_user_best'
-// export const USER_BESTS = '/user_bests'
+// export const USER_BESTS = '/api/get_user_best'
+export const USER_BESTS = '/user_bests'
 
-export const USER_RECENTS = '/api/get_user_recent'
-// export const USER_RECENTS = '/user_recents'
+// export const USER_RECENTS = '/api/get_user_recent'
+export const USER_RECENTS = '/user_recents'
 
-export const USERS = '/api/get_user'
-// export const USERS = '/users'
+// export const USERS = '/api/get_user'
+export const USERS = '/users'
 
