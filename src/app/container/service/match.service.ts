04eb41e1bcf5227958f808078a39a4bc8820193d
Göcs Jëss <aaaaeinai@gmail.com>
2018-02-27 18:00:02 +0800

    added key substring

--- src/app/container/service/match.service.ts
+++ src/app/container/service/match.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Match, Game } from "../model/match";
 import { Observable } from 'rxjs/Observable';
 import { OSU_API, MATCHES } from "./links-const";
+import { KEY } from "./key";
 
 @Injectable()
 export class MatchesService {
@@ -13,18 +14,18 @@ export class MatchesService {
     ) { }
 
     getMatchesAndGames(data: (matches: { match: Match, games: Game[] }[]) => void) {
-        return this.http.get(OSU_API + MATCHES)
+        return this.http.get(OSU_API + MATCHES + '?k=' + KEY)
             .subscribe(data)
     }
 
     getMatches(data: (matches: Match[]) => void) {
-        return this.http.get(OSU_API + MATCHES)
+        return this.http.get(OSU_API + MATCHES + '?k=' + KEY)
             .subscribe(data)
     }
 
     // TODO: Games http logic goes here
     getGames(games: Game[]) {
-        return this.http.get(OSU_API + MATCHES)
+        return this.http.get(OSU_API + MATCHES + '?k=' + KEY)
             .subscribe((data: Match[]) => {
                 data.forEach((match: Match) => {
                     match["games"].forEach((matchGames: Game[]) => {
