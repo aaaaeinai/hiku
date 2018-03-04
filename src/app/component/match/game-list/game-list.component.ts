400b6a6251c052e89554c038f9d2c5d3fa4c6cbc
Göcs Jëss <aaaaeinai@gmail.com>
2018-03-04 17:50:12 +0800

    finished gamelist

--- src/app/component/match/game-list/game-list.component.ts
+++ src/app/component/match/game-list/game-list.component.ts
@@ -1,19 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Game } from "../../../container/model/match";
 import { MatchesService } from "../../../container/service/match.service";
+import { ActivatedRoute, Params } from "@angular/router";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
     selector: 'app-game-list',
     templateUrl: './game-list.component.html'
 })
-export class GameListComponent {
+export class GameListComponent implements OnInit, OnDestroy {
 
     gameList: Game[]
+    dispose: Subscription
 
     constructor(
-        private ms: MatchesService
+        private ms: MatchesService,
+        private route: ActivatedRoute
     ) {
-        console.log('entered')
-        this.ms.getGames(this.gameList)
+    }
+    ngOnInit() {
+        this.dispose = this.route.params
+            .subscribe((params: Params) => {
+                this.ms.getGames(gl => this.gameList = gl[params['id']]['games'])
+            })
+    }
+
+    ngOnDestroy() {
+        this.dispose.unsubscribe()
     }
 }
